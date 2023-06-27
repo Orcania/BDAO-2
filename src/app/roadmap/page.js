fd4961a01da2c196794e9aa53b7f8f447d1faf98
Ghadi <yobiz668@gmail.com>
2023-06-27 17:42:57 +0300

    footer fixes

--- src/app/roadmap/page.js
+++ src/app/roadmap/page.js
@@ -99,7 +99,6 @@ export default function useRoadmap() {
                     <br />
                     {renderContent()}
                 </div>
-                <Footer />
             </div>
         </>
     );
