829284fba71b95b2d9d897ee6f6f3ca4434eb806
loque18 <madridx2010@gmail.com>
2023-07-22 14:52:04 -0500

    feat: unity

---
+++ src/app/treasury/page.jsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <main>
+            <div className="contaainer max-auto ">
+                <iframe
+                    src="/unity/unity.html"
+                    className="w-full aspect-video border-none"
+                ></iframe>
+            </div>
+        </main>
+    );
+}
