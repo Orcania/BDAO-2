bff741a236d9b60426a7cd54cccdfc1a965329bf
loque18 <madridx2010@gmail.com>
2023-07-28 16:02:27 -0500

    feat: updated unity page

--- src/app/treasury/page.jsx
+++ src/app/treasury/page.jsx
@@ -2,11 +2,10 @@ export default function Page() {
     return (
         <main>
             <div className="h-[calc(100vh-82px)]  ">
-                <div className="container mx-auto h-full  flex justify-center items-center">
-                    <iframe
-                        src="/unity/unity.html"
-                        className="h-full aspect-video border-none"
-                    ></iframe>
+                <div className="h-full bg-[blue]  flex justify-center items-center">
+                    <iframe src="/unity/unity.html" className="w-full h-full border-none "></iframe>
+
+                    {/* <div className="bg-purple-500 h-full aspect-video"></div> */}
                 </div>
             </div>
         </main>
